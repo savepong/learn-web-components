625a67ead876dcfb506391c0576421c64d9733aa
Savepong <savepong.com@gmail.com>
2020-12-12 20:13:01 +0700

    Adding a Render method

--- components/tooltip.js
+++ components/tooltip.js
@@ -1,8 +1,8 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    this._tooltipContainer;
     this._tooltipIcon;
+    this._tooltipVisible = false;
     this._tooltipText = "Some dummy tooltip text.";
     this.attachShadow({ mode: "open" });
 
@@ -60,6 +60,7 @@ class Tooltip extends HTMLElement {
       this._hideTooltip.bind(this)
     );
     this.style.position = "relative";
+    this._render();
   }
 
   disconnectedCallback() {
@@ -80,14 +81,27 @@ class Tooltip extends HTMLElement {
     return ["text"];
   }
 
+  _render() {
+    let tooltipContainer = this.shadowRoot.querySelector("div");
+    if (this._tooltipVisible) {
+      tooltipContainer = document.createElement("div");
+      tooltipContainer.textContent = this._tooltipText;
+      this.shadowRoot.appendChild(tooltipContainer);
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer);
+      }
+    }
+  }
+
   _showTooltip() {
-    this._tooltipContainer = document.createElement("div");
-    this._tooltipContainer.textContent = this._tooltipText;
-    this.shadowRoot.appendChild(this._tooltipContainer);
+    this._tooltipVisible = true;
+    this._render();
   }
 
   _hideTooltip() {
-    this.shadowRoot.removeChild(this._tooltipContainer);
+    this._tooltipVisible = false;
+    this._render();
   }
 }
 
